c6000fe7b42b93da442d20a9b4092107801ea638
joemeyer1 <47130044+joemeyer1@users.noreply.github.com>
2025-05-02 02:40:13 +0100

    def Environment.is_ok(behavior)

--- src/environments/environment.py
+++ src/environments/environment.py
@@ -1,4 +1,10 @@
 
+from typing import Tuple
+
 
 class Environment:
-    pass
+    low_point: float
+    high_point: float
+
+    def is_ok(self, behavior) -> bool:
+        return self.low_point <= behavior <= self.high_point
