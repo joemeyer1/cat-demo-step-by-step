ba7e286e51f3f9e6ce59ad254300eb6f1bf2dbf9
joemeyer1 <47130044+joemeyer1@users.noreply.github.com>
2025-05-02 02:58:26 +0100

    implement Cat.train()

--- src/animals/cat.py
+++ src/animals/cat.py
@@ -1,13 +1,13 @@
 
 import random
-from typing import Tuple
+from typing import List
 
 from src.environments.environment import Environment
 from src.utils import ask_chatgpt
 
 
 class Cat:
-    restrict_behavior_to_range: Tuple = (0, 1)
+    restrict_behavior_to_range: List[float, float] = [0, 1]
 
     def generate_random_restricted_behavior(self, environment: Environment) -> float:
         return random.uniform(*self.restrict_behavior_to_range)
@@ -15,8 +15,13 @@ class Cat:
     def behave(self, environment: Environment) -> float:
         return self.generate_random_restricted_behavior(environment)
 
-    def train(self, environment: Environment):
-        pass
+    def train(self, environment: Environment, n_epochs: int = 100):
+        for i in range(n_epochs):
+            behavior = self.generate_random_restricted_behavior(environment)
+            if ask_chagpt.is_behavior_too_low(behavior=behavior, environment=environment):
+                self.restrict_behavior_to_range[0] += 0.1
+            elif ask_chagpt.is_behavior_too_high(behavior=behavior, environment=environment):
+                self.restrict_behavior_to_range[1] -= 0.1
 
     def test(self, environment: Environment):
         behavior = self.behave(environment)
