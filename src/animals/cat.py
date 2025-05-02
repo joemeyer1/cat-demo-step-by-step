8f555d7e2785f9799dd144af9aba5dfed2679fae
joemeyer1 <47130044+joemeyer1@users.noreply.github.com>
2025-05-02 02:28:12 +0100

    def generate_random_restricted_behavior()

--- src/animals/cat.py
+++ src/animals/cat.py
@@ -4,8 +4,12 @@ from src.environments.environment import Environment
 
 
 class Cat:
+    restrict_behavior_to_range = (0, 1)
 
-    def behave(self, environment: Environment):
+    def generate_random_restricted_behavior(self, environment) -> float:
+        pass
+
+    def behave(self, environment: Environment) -> float:
         return self.generate_random_restricted_behavior(environment)
 
     def train(self, environment: Environment):
