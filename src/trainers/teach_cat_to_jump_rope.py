a6255ac18621789bfb82b5aa0d8640740b222c33
joemeyer1 <47130044+joemeyer1@users.noreply.github.com>
2025-05-02 02:16:43 +0100

    Update teach_cat_to_jump_rope.py
    fix a bug

--- src/trainers/teach_cat_to_jump_rope.py
+++ src/trainers/teach_cat_to_jump_rope.py
@@ -5,7 +5,7 @@ from src.environments.jump_rope import JumpRope
 from src.utils.environment_data import EnvironmentData
 
 
-def teach_cat_to_jump_rope(cat: Cat, jump_rope: WaterBody) -> Cat:
+def teach_cat_to_jump_rope(cat: Cat, jump_rope: JumpRope) -> Cat:
 
     jump_rope_data = EnvironmentData(environment=jump_rope, train_test_proportion=0.8)  # use 80% of data for training, 20% for testing
     cat.train(jump_rope_data.train_environment)
