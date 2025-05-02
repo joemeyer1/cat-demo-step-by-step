fc80b1a7254f4ff2991292bda706618009934b7a
joemeyer1 <47130044+joemeyer1@users.noreply.github.com>
2025-05-02 02:08:56 +0100

    Create birth_a_skilled_cat.py

---
+++ birth_a_skilled_cat.py
@@ -0,0 +1,15 @@
+
+from src.animals.cat import Cat
+from src.environments.jump_rope import JumpRope
+from src.environments.water_body import WaterBody
+from src.trainers.teach_cat_to_swim import teach_cat_to_swim
+from src.trainers.teach_cat_to_jump_rope import teach_cat_to_jump_rope
+
+
+def birth_a_skilled_cat() -> Cat:
+    """Creates a cat and teaches it to do stuff."""
+
+    cat = Cat()
+    cat = teach_cat_to_swim(cat=cat, body_of_water=WaterBody())
+    cat = teach_cat_to_jump_rope(cat=cat, jump_rope=JumpRope())
+    return cat
