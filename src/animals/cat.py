cf86a1fc81c715346d9114f6c56cdd182f14e421
joemeyer1 <47130044+joemeyer1@users.noreply.github.com>
2025-05-02 02:26:01 +0100

    Update cat.py
    def behave(environment)

--- src/animals/cat.py
+++ src/animals/cat.py
@@ -4,6 +4,10 @@ from src.environments.environment import Environment
 
 
 class Cat:
+
+    def behave(self, environment: Environment):
+        return self.generate_random_restricted_behavior(environment)
+
     def train(self, environment: Environment):
         pass
 
