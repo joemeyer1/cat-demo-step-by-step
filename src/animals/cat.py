8e22b5dd0bc55399ebbe4655a9618d435f502d87
joemeyer1 <47130044+joemeyer1@users.noreply.github.com>
2025-05-02 02:20:34 +0100

    Update cat.py
    initialize empty train, test methods

--- src/animals/cat.py
+++ src/animals/cat.py
@@ -1,4 +1,11 @@
 
 
+from src.environments.environment import Environment
+
+
 class Cat:
-    pass
+    def train(self, environment: Environment):
+        pass
+
+    def test(self, environment: Environment):
+        pass
