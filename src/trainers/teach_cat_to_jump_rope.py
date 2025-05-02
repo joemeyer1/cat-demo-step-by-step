cc2de8c5865f0fde159c9c5fc5934e086a3a1475
joemeyer1 <47130044+joemeyer1@users.noreply.github.com>
2025-05-02 02:15:35 +0100

    Create teach_cat_to_jump_rope.py

---
+++ src/trainers/teach_cat_to_jump_rope.py
@@ -0,0 +1,13 @@
+
+
+from src.animals.cat import Cat
+from src.environments.jump_rope import JumpRope
+from src.utils.environment_data import EnvironmentData
+
+
+def teach_cat_to_jump_rope(cat: Cat, jump_rope: WaterBody) -> Cat:
+
+    jump_rope_data = EnvironmentData(environment=jump_rope, train_test_proportion=0.8)  # use 80% of data for training, 20% for testing
+    cat.train(jump_rope_data.train_environment)
+    cat.test(jump_rope_data.test_environment)
+    return cat
